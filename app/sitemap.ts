e638d61b2df7650b2b1bde4f9516c9c074ffc5c1
iamfaisalpk <iamfaizy123@gmail.com>
2025-11-20 16:04:35 +0530

    Seo friendly

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,28 +3,10 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "https://faisal-faizypk.vercel.app/",
+            url: "https://faisal-faizypk.vercel.app",
             lastModified: new Date(),
             changeFrequency: "monthly",
             priority: 1.0,
-        },
-        {
-            url: "https://faisal-faizypk.vercel.app/#about",
-            lastModified: new Date(),
-            changeFrequency: "yearly",
-            priority: 0.8,
-        },
-        {
-            url: "https://faisal-faizypk.vercel.app/#projects",
-            lastModified: new Date(),
-            changeFrequency: "yearly",
-            priority: 0.8,
-        },
-        {
-            url: "https://faisal-faizypk.vercel.app/#contact",
-            lastModified: new Date(),
-            changeFrequency: "yearly",
-            priority: 0.8,
         },
     ];
 }
